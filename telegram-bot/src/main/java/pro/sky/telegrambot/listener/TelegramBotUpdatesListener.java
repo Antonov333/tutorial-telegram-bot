76c3faf94de7222a1856de7648ccbc14aec323e9
Antonov333 <antonovsergei333@yahoo.com>
2023-10-06 13:26:36 +0300

    User input parsing composed

--- telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -16,6 +16,7 @@ import pro.sky.telegrambot.repository.NotyRepository;
 import javax.annotation.PostConstruct;
 import javax.swing.text.html.parser.Entity;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
@@ -24,7 +25,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private final NotyRepository noties;
 
-    private enum Command {START, HELP, VIEW, SET, UNCLEAR}
+    private enum Command {START, HELP, VIEW, SET, SET_IMPLICIT, EDIT, DELETE, UNCLEAR}
 
     ;
 
@@ -58,8 +59,9 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             logger.info("Processing update: {}", update);
 
             long chatId = update.message().chat().id();
-            System.out.println("noties.findAll() = " + noties.findAll());
-            switch (update.message().text()) {
+            String string = update.message().text();
+            System.out.println("considerCommand(string) = " + considerCommand(string));
+            switch (string) {
                 case "/start": {
                     noties.save(new Noty(0L, chatId, "/start command received"));
                     sendMessage(chatId, startText);
@@ -103,4 +105,132 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         return null;
     }
 
+    private Command considerCommand(String userInput) {
+        if (userInput == null) {
+            return Command.UNCLEAR;
+        }
+        if (userInput.isEmpty()) {
+            return Command.UNCLEAR;
+        }
+        String input = userInput.toLowerCase();
+
+        if (input.startsWith("/start")) {
+            return Command.START;
+        }
+
+        if (input.startsWith("/help")) {
+            return Command.HELP;
+        }
+        if (input.startsWith("/view")) {
+            return Command.VIEW;
+        }
+        if (input.startsWith("/set") & input.length() > 19) {
+            return Command.SET;
+        }
+
+        if (input.startsWith("/delete")) {
+            return Command.DELETE;
+        }
+
+        if (setImplicitRecognize(input).isSetImplicit) {
+            return Command.SET_IMPLICIT;
+        }
+
+        return Command.UNCLEAR;
+    }
+
+    private static class SetImplicitRecognition {
+
+        private boolean isSetImplicit;
+        private String dateString;
+        private String timeString;
+        private String taskDescription;
+
+        SetImplicitRecognition() {
+            isSetImplicit = false;
+
+        }
+
+        public boolean isSetImplicit() {
+            return isSetImplicit;
+        }
+
+        public void setSetImplicit(boolean setImplicit) {
+            isSetImplicit = setImplicit;
+        }
+
+        public String getDateString() {
+            return dateString;
+        }
+
+        public void setDateString(String dateString) {
+            this.dateString = dateString;
+        }
+
+        public String getTimeString() {
+            return timeString;
+        }
+
+        public void setTimeString(String timeString) {
+            this.timeString = timeString;
+        }
+
+        public String getTaskDescription() {
+            return taskDescription;
+        }
+
+        public void setTaskDescription(String taskDescription) {
+            this.taskDescription = taskDescription;
+        }
+
+        @Override
+        public String toString() {
+            return "SetImplicitRecognition{" +
+                    "isSetImplicit=" + isSetImplicit +
+                    ", dateString='" + dateString + '\'' +
+                    ", timeString='" + timeString + '\'' +
+                    ", taskDescription='" + taskDescription + '\'' +
+                    '}';
+        }
+    }
+
+    private SetImplicitRecognition setImplicitRecognize(String userInput) {
+
+        SetImplicitRecognition result = new SetImplicitRecognition();
+
+        if (userInput == null || (userInput.length() < 16)) {
+            return result;
+        }
+
+        Pattern hoursAndMinutes = Pattern.compile("(([01][0-9])|(2[0-3])):[0-5][0-9]?");
+
+        Pattern dayMonthYear = Pattern.compile(
+                "(([3][01])|([0-2]\\d))\\.\\d\\d\\.2\\d{3}"
+        );
+
+        String userInputHead = userInput.substring(0, 16);
+        String date = "";
+        String time = "";
+
+        Matcher matcher = hoursAndMinutes.matcher(userInputHead);
+        Boolean hasTime = matcher.find();
+        if (hasTime) {
+            time = matcher.group();
+        }
+
+        matcher = dayMonthYear.matcher(userInputHead);
+        Boolean hasDate = matcher.find();
+        if (hasDate) {
+            date = matcher.group();
+        }
+        ;
+
+        if (hasTime & hasDate) {
+            result.setSetImplicit(true);
+            result.setDateString(date);
+            result.setTimeString(time);
+            result.setTaskDescription(userInput.substring(16));
+        }
+        return result;
+    }
 }
